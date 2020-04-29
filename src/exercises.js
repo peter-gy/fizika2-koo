265a6f023c4ed8a92b8ae47bf9d921cb865986b1
Péter Gyarmati <gyarmatipeti01@gmail.com>
2020-04-29 15:26:37 +0200

    created model

--- src/exercises.js
+++ src/exercises.js
@@ -1,3 +1,44 @@
+class Exercise {
+
+    /**
+     * 
+     * @param {String}    description description as simple string 
+     * @param {Formula[]} formulas    an array of Formulas associated with this exercise
+     */
+    constructor(description, formulas) {
+        this.description = description
+        this.formulas = formulas
+    }
+}
+
+class Formula {
+
+    /**
+     * 
+     * @param {String}     description description as simple string
+     * @param {String}     display     display as latex string
+     * @param {Variable[]} variables   an array of Variables constituting this formula
+     */
+    constructor(description, display, variables) {
+        this.description = description
+        this.display = display
+        this.variables = variables
+    }
+}
+
+class Variable {
+
+    /**
+     * 
+     * @param {String} description description as simple string
+     * @param {String} display     display as latex string
+     */
+    constructor(description, display) {
+        this.description = description
+        this.display = display
+    }
+}
+
 const ex_1_3_script = 
 `
 @interact
