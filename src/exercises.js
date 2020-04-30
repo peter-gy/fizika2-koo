58c2011b3ab906d25c1d6e6b3f0e54215cb8c5c8
Péter Gyarmati <gyarmatipeti01@gmail.com>
2020-04-30 11:18:35 +0200

    removed unnecessary fields from Formula class

--- src/exercises.js
+++ src/exercises.js
@@ -16,18 +16,16 @@ class Formula {
 
     /**
      * 
-     * @param {String}     description description as simple string
-     * @param {String}     display     display as simple string
+     * @param {String}     display     display as simple string (eg.: r^2*pi*h)
      * @param {Variable}   lhs         a single Variable, the left-hand side of the formula
      * @param {Variable[]} rhs         an array of Variables, the right-hand side of the formula
      */
-    constructor(description, display, lhs, rhs) {
-        this.description = description
+    constructor(display, lhs, rhs) {
         this.display = display
         this.lhs = lhs
         this.rhs = rhs
         
-        // generating script
+        // generating Sage Math python script
         const param_list = rhs.map((vrbl, idx) => `arg${idx}=('${vrbl.description} $${vrbl.display} [${vrbl.unit}]$', 1.0)`).join(',');
         const args = rhs.map((vrbl, idx) => `arg${idx}`).join(',')
         const vars = rhs.map(vrbl => `${vrbl.display}`).join(',')
@@ -36,7 +34,7 @@ class Formula {
 @interact
 def _(${param_list}):
     var('${vars}')
-    show('${description}')
+    show('${lhs.description}')
     func(${vars}) = ${display}
     show('${lhs.display} = ' + latex(func(${vars})))
     show('${lhs.display}, ${lhs.unit}: ' + latex(func(${args}).n()))
@@ -49,9 +47,9 @@ class Variable {
 
     /**
      * 
-     * @param {String} description description as simple string
-     * @param {String} display     display as simple string
-     * @param {String} unit        unit of measure as simple string
+     * @param {String} description description as simple string (eg.: Volume of a cylinder)
+     * @param {String} display     display as simple string (eg.: V)
+     * @param {String} unit        unit of measure as simple string (eg.: m3)
      */
     constructor(description, display, unit) {
         this.description = description
@@ -62,4 +60,6 @@ class Variable {
 
 let var_A = new Variable('csővezeték átlagos keresztmetszete', 'A', 'm2')
 let var_d = new Variable('átmérő', 'd', 'm')
-let f = new Formula('csővezeték átlagos keresztmetszete', '1/4*(pi)*d^2', var_A, [var_d])
+let var_l = new Variable('hosszúság', 'l', 'm')
+
+let f = new Formula('1/4*(pi)*d^2', var_A, [var_d])
