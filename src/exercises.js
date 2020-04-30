68768a7dd5fc05aab8f8c9b1bf240530975657db
Péter Gyarmati <gyarmatipeti01@gmail.com>
2020-04-30 11:03:35 +0200

    implemented SageMath sxript templating

--- src/exercises.js
+++ src/exercises.js
@@ -17,17 +17,32 @@ class Formula {
     /**
      * 
      * @param {String}     description description as simple string
-     * @param {String}     display     display as latex string
+     * @param {String}     display     display as simple string
      * @param {Variable}   lhs         a single Variable, the left-hand side of the formula
      * @param {Variable[]} rhs         an array of Variables, the right-hand side of the formula
-     * @param {Stirng}     script      SageMath script to solve this formula
      */
     constructor(description, display, lhs, rhs) {
         this.description = description
         this.display = display
         this.lhs = lhs
         this.rhs = rhs
+        
+        // generating script
+        const param_list = rhs.map((vrbl, idx) => `arg${idx}=('${vrbl.description} $${vrbl.display} [${vrbl.unit}]$', 1.0)`).join(',');
+        const args = rhs.map((vrbl, idx) => `arg${idx}`).join(',')
+        const vars = rhs.map(vrbl => `${vrbl.display}`).join(',')
+        this.script = 
+`
+@interact
+def _(${param_list}):
+    var('${vars}')
+    show('${description}')
+    func(${vars}) = ${display}
+    show('${lhs.display} = ' + latex(func(${vars})))
+    show('${lhs.display}, ${lhs.unit}: ' + latex(func(${args}).n()))
+`
     }
+
 }
 
 class Variable {
@@ -35,31 +50,16 @@ class Variable {
     /**
      * 
      * @param {String} description description as simple string
-     * @param {String} display     display as latex string
-     * @param {String} unit        unit of measure as latex string
+     * @param {String} display     display as simple string
+     * @param {String} unit        unit of measure as simple string
      */
     constructor(description, display, unit) {
         this.description = description
         this.display = display
+        this.unit = unit
     }
 }
 
-const ex_1_3_script = 
-`
-@interact
-def _(a=(1, 10)):
-    print(factorial(a))
-`
-
-const ex_1_4_script = 
-`
-@interact
-def _(a=(1, 10)):
-    print(a**a)
-`
-
-export const exercises = 
-[
-    ex_1_3_script,
-    ex_1_4_script
-]
+let var_A = new Variable('csővezeték átlagos keresztmetszete', 'A', 'm2')
+let var_d = new Variable('átmérő', 'd', 'm')
+let f = new Formula('csővezeték átlagos keresztmetszete', '1/4*(pi)*d^2', var_A, [var_d])
